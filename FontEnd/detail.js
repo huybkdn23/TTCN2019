5ad1ab09d98367f90fa64e25dea28288c06df79a
LocNguyenEnlab <loc.nguyen@enlabsoftware.com>
2019-10-26 22:37:50 +0700

    update detail page

--- FontEnd/detail.js
+++ FontEnd/detail.js
@@ -8,12 +8,27 @@ function onLoad() {
     var id = sessionStorage.getItem('id');
     var detail = new LocationDetail({
         title: '6 Lake District locations to visit this autumn',
-        content: 'Since being awarded UNESCO World Heritage status in 2017, the popularity of the Lake District National Park in north-west England has continued to soar. While the summer is, undoubtedly, a lovely time to visit, savvy travellers will find the Lakes and their towns and villages are an equally beautiful destination in the autumn. The scenery is ablaze with colour, the summer crowds have thinned out and there’s plenty to see and do during, no matter the weather.'
+        content: 'Since being awarded UNESCO World Heritage status in 2017, the popularity of the Lake District National Park in north-west England has continued to soar. While the summer is, undoubtedly, a lovely time to visit, savvy travellers will find the Lakes and their towns and villages are an equally beautiful destination in the autumn. The scenery is ablaze with colour, the summer crowds have thinned out and there’s plenty to see and do during, no matter the weather.',
+        imageLink: [
+            'images/anh.jpg',
+            'https://southeastasiabackpacker.com/wp-content/uploads/2018/01/Halong-bay-autumn.jpg',
+            'https://vietnam.travel/sites/default/files/360Tour/HaLong/socialThumbnail2.jpg'
+        ]
     });
     var h1 = document.createElement('h1');
+    h1.setAttribute('style', 'color: #2689e7');
     h1.innerHTML = detail.title;
     var p = document.createElement('p');
     p.innerHTML = detail.content;
+    p.setAttribute('style', 'color: #000');
     document.getElementById('detail').appendChild(h1);
     document.getElementById('detail').appendChild(p);
+    var slideDiv = document.getElementById('slide');
+    for (var _i = 0, _a = detail.imageLink; _i < _a.length; _i++) {
+        var imageLink = _a[_i];
+        var div = document.createElement('div');
+        div.setAttribute('class', 'intro-section');
+        div.setAttribute('style', "background-image: url('" + imageLink + "')");
+        slideDiv.appendChild(div);
+    }
 }
--- FontEnd/detail.ts
+++ FontEnd/detail.ts
@@ -2,6 +2,7 @@ class LocationDetail {
     id: number;
     title: string;
     content: string;
+    imageLink: string[];
     constructor(init?: Partial<LocationDetail>) {
         (<any>Object).assign(this, init);
     }
@@ -11,12 +12,27 @@ function onLoad() {
     const id = sessionStorage.getItem('id');
     const detail = new LocationDetail({
         title: '6 Lake District locations to visit this autumn',
-        content: 'Since being awarded UNESCO World Heritage status in 2017, the popularity of the Lake District National Park in north-west England has continued to soar. While the summer is, undoubtedly, a lovely time to visit, savvy travellers will find the Lakes and their towns and villages are an equally beautiful destination in the autumn. The scenery is ablaze with colour, the summer crowds have thinned out and there’s plenty to see and do during, no matter the weather.'
+        content: 'Since being awarded UNESCO World Heritage status in 2017, the popularity of the Lake District National Park in north-west England has continued to soar. While the summer is, undoubtedly, a lovely time to visit, savvy travellers will find the Lakes and their towns and villages are an equally beautiful destination in the autumn. The scenery is ablaze with colour, the summer crowds have thinned out and there’s plenty to see and do during, no matter the weather.',
+        imageLink: [
+            'images/anh.jpg',
+            'https://southeastasiabackpacker.com/wp-content/uploads/2018/01/Halong-bay-autumn.jpg',
+            'https://vietnam.travel/sites/default/files/360Tour/HaLong/socialThumbnail2.jpg'
+        ]
     });
     const h1 = document.createElement('h1');
+    h1.setAttribute('style', 'color: #2689e7');
     h1.innerHTML = detail.title;
     const p = document.createElement('p');
     p.innerHTML = detail.content;
+    p.setAttribute('style', 'color: #000');
     document.getElementById('detail').appendChild(h1);
     document.getElementById('detail').appendChild(p);
+
+    var slideDiv = document.getElementById('slide');
+    for (const imageLink of detail.imageLink) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'intro-section');
+        div.setAttribute('style', "background-image: url('" + imageLink + "')");
+        slideDiv.appendChild(div);
+    }
 }
